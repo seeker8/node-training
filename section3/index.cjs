db28879c429e3280d2475309b79b591080a7038b
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-02-23 18:10:31 -0500

    demonstrate the usage of process  * show how to get input form the user through args passed when executing our script

--- section3/index.cjs
+++ section3/index.cjs
@@ -1,5 +1,11 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.cjs');
 
-console.log(getNotes());
-console.log(chalk.green('Sueccess'));
+const command = process.argv[2];
+
+if (command === 'add') {
+  console.log('Adding a note!');
+}
+else if (command === 'remove') {
+  console.log('Removing a note!');
+}
