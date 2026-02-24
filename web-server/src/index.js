3f64ecbcc2a2518fc269db36af291f3e092ab689
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-02-24 18:52:32 -0500

    web-server: use handlebars partials

--- web-server/src/index.js
+++ web-server/src/index.js
@@ -1,14 +1,19 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+
+// setup handlebars configuration
 
 const app = express();
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // setup handlebars
 app.set('view engine', 'hbs');
 // set views directory. By default it looks for 'views' directory
 app.set('views', viewsPath);
 app.use(express.static(path.join(__dirname, '../public')));
+hbs.registerPartials(partialsPath);
 
 app.get('', (req, res) => {
   res.render('index', { title: 'Cool Weather App' });
