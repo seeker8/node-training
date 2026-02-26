1e60326cfc5874832b0e22f1eab4461eae9c9a2c
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-02-26 17:56:14 -0500

    add update enpoint for users and tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -45,6 +45,22 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
+app.patch('/users/:id', async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      returnDocument: 'after',
+      runValidators: true
+    });
+    if (!updatedUser) {
+      res.status(404).send(updatedUser);
+    }
+    res.send(updatedUser);
+  }
+  catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 app.post('/tasks', async (req, res) => {
   try {
     const task = new Task(req.body);
@@ -76,7 +92,30 @@ app.get('/tasks/:id', async (req, res) => {
     res.send(task);
   }
   catch (error) {
-    res.status(500).send();
+    res.status(500).send(error);
+  }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const acceptedUpdated = ['description', 'completed'];
+  const isValidOperation = updates.every(updt => acceptedUpdated.includes(updt));
+  if (!isValidOperation) {
+    res.status(400).send({ error: 'Invalid update' });
+  }
+
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      returnDocument: 'after',
+      runValidators: true
+    });
+    if (!updatedTask) {
+      return res.status(404).send()
+    }
+    res.send(updatedTask);
+  }
+  catch (error) {
+    res.status(500).send(error);
   }
 });
 
