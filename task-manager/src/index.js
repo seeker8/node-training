dd08acb38379dd5350e4fda3bdecfc053a4b6beb
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-02-26 14:10:18 -0500

    add endpoint to get a single user

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -19,6 +19,30 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+  console.log(req.params.id);
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send()
+      }
+      res.send(user);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task.save()
