c86e40a6e3a2339e5a68962579c1b19e7f1ae5f2
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-02-26 17:12:37 -0500

    update tasks endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -45,32 +45,39 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
-app.post('/tasks', (req, res) => {
-  const task = new Task(req.body);
-  task.save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
+app.post('/tasks', async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    const newTask = await task.save();
+    res.status(201).send(newTask);
+  }
+  catch (error) {
+    res.status(400).send(error);
+  }
+
 });
 
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then(tasks => res.send(tasks))
-    .catch((error) => res.status(500).send());
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  }
+  catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.get('/tasks/:id', (req, res) => {
-  Task.findById(req.params.id)
-    .then(task => {
-      if (!task) {
-        return res.status(404).send();
-      }
-      res.send(task);
-    })
-    .catch((error) => res.status(500).send());
+app.get('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  }
+  catch (error) {
+    res.status(500).send();
+  }
 });
 
 
