9e4fb9cd53b9e0335b925db72c7b92bd5b922eed
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-03-04 18:26:22 -0500

    initial setup for socket.io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,12 +1,22 @@
 import express from 'express';
+import http from 'node:http';
 import path from 'node:path';
+import { Server } from 'socket.io';
 
 const __dirname = import.meta.dirname;
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+// create socketio instance
+const io = new Server(server);
+
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('connected');
+});
+
+server.listen(port, () => {
   console.log('listening on port 3000...');
 });
