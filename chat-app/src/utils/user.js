20174eaa800b98601f5df4e5845978207d44ed67
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-03-06 13:27:31 -0500

    add utility function to manage users in rooms and add some tests for those utility functions

---
+++ chat-app/src/utils/user.js
@@ -0,0 +1,41 @@
+
+const users = [];
+
+export const addUser = ({ id, username, room }) => {
+  const userName = username.trim().toLowerCase();
+  const joinedRoom = room.trim().toLowerCase();
+  if (!userName || !joinedRoom) {
+    return {
+      error: 'User name and room are required'
+    };
+  }
+
+  // check for existing user
+  const existingUser = users.find(user => user.userName === userName && user.joinedRoom == joinedRoom);
+
+  if (existingUser) {
+    return {
+      error: 'User name is in use'
+    };
+  }
+
+  // Store user
+  const user = { id, userName, joinedRoom };
+  users.push(user);
+  return user;
+};
+
+export const removeUser = (id) => {
+  const index = users.findIndex(user => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+export const getUser = (id) => {
+  return users.find(user => user.id === id);
+};
+
+export const getUsersInRoom = (room) => {
+  return users.filter(user => user.joinedRoom === room);
+};
---
+++ chat-app/test/user.spec.js
@@ -0,0 +1,59 @@
+import { beforeEach, test, expect } from 'vitest';
+import { addUser, removeUser, getUser, getUsersInRoom } from '../src/utils/user.js';
+
+
+beforeEach(() => {
+  const users = [
+    {
+      id: 12,
+      username: 'Aira',
+      room: 'dandadan'
+    },
+    {
+      id: 14,
+      username: 'Jiji',
+      room: 'dandadan'
+    },
+    {
+      id: 16,
+      username: 'Miramira',
+      room: 'withcriv'
+    },
+    {
+      id: 18,
+      username: 'Nona',
+      room: 'withcriv'
+    }
+  ];
+  for (const user of users) {
+    addUser(user);
+  }
+});
+
+test('add user', () => {
+  const user = addUser({
+    id: 23,
+    username: 'Okarun',
+    room: 'dandadan'
+  });
+  expect(user).not.toBeNull();
+  expect(user.userName).toBe('okarun');
+});
+
+test('remove user', () => {
+  const removed = removeUser(23);
+  expect(removed).not.toBeNull();
+  expect(removed.userName).toBe('okarun');
+});
+
+test('get user', () => {
+  const user = getUser(18);
+  expect(user.userName).toBe('nona');
+});
+
+test('get user in room', () => {
+  const usersInRoom = getUsersInRoom('withcriv');
+  const usersInDandadan = getUsersInRoom('dandadan');
+  expect(usersInRoom.length).toBe(2);
+  expect(usersInDandadan.length).toBe(2);
+})
